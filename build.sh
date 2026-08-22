5d4a4dd13fcc24673bae5a68bfb983b42fe83da1
mswetha442 <mswetha442@gmail.com>
2026-08-22 11:44:46 +0530

    Force set superuser credentials in build script

--- build.sh
+++ build.sh
@@ -7,7 +7,18 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Auto-create superuser using environment variables
-if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
-  python manage.py createsuperuser --no-input || true
-fi
+# Create or reset superuser credentials
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+username = '$DJANGO_SUPERUSER_USERNAME';
+email = '$DJANGO_SUPERUSER_EMAIL';
+password = '$DJANGO_SUPERUSER_PASSWORD';
+if username and password:
+    user, created = User.objects.get_or_create(username=username, defaults={'email': email, 'is_staff': True, 'is_superuser': True});
+    user.set_password(password);
+    user.is_staff = True;
+    user.is_superuser = True;
+    user.save();
+    print('Superuser updated successfully!');
+"
