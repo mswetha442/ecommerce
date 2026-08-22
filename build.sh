e016ddda6d3a440d7d4a60bb43f12c57fee251d0
mswetha442 <mswetha442@gmail.com>
2026-08-22 11:13:23 +0530

    Update build script for automatic superuser creation

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
-
+python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Auto-create superuser using environment variables
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+  python manage.py createsuperuser --no-input || true
+fi
