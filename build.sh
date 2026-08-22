e8db62d0ea0d6ce10d556a635a142245b051a3c1
mswetha442 <mswetha442@gmail.com>
2026-08-22 11:51:13 +0530

    Hardcode superuser reset

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
@@ -7,18 +6,19 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Create or reset superuser credentials
 python manage.py shell -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
-username = '$DJANGO_SUPERUSER_USERNAME';
-email = '$DJANGO_SUPERUSER_EMAIL';
-password = '$DJANGO_SUPERUSER_PASSWORD';
-if username and password:
-    user, created = User.objects.get_or_create(username=username, defaults={'email': email, 'is_staff': True, 'is_superuser': True});
-    user.set_password(password);
-    user.is_staff = True;
-    user.is_superuser = True;
-    user.save();
-    print('Superuser updated successfully!');
+username = 'swetha';
+email = 'swetha@example.com';
+password = 'MyStrongPassword123!';
+
+user, _ = User.objects.get_or_create(username=username, defaults={'email': email});
+user.email = email;
+user.set_password(password);
+user.is_staff = True;
+user.is_superuser = True;
+user.is_active = True;
+user.save();
+print('ADMIN USER SUCCESSFULLY CREATED/UPDATED');
 "
